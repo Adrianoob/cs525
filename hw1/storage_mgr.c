e7fc8c93f81dc3d4401300e0319057ecb63e53d2
Adrianoob <Adrianoob@users.noreply.github.com>
2016-09-26 14:21:28 -0500

    Update storage_mgr.c

--- hw1/storage_mgr.c
+++ hw1/storage_mgr.c
@@ -5,22 +5,22 @@ extern void initStorageManager (void) {
 	// initialize SM
 }
 
-// extern RC createPageFile (char *fileName) {
-// 	FILE *pagef = fopen(fileName, "w");
-//     char *totalpg_str, *first_page;
+extern RC createPageFile (char *fileName) {
+	FILE *pagef = fopen(fileName, "w");
+    char *totalpg_str, *first_page;
 
-//     totalpg_str = (char *) calloc(PAGE_SIZE, sizeof(char));  /* allocate "first" page to store total number of pages information */
-//     first_page = (char *) calloc(PAGE_SIZE, sizeof(char));   /* considered as actual first page for the data */
+    totalpg_str = (char *) calloc(PAGE_SIZE, sizeof(char));  /* allocate "first" page to store total number of pages information */
+    first_page = (char *) calloc(PAGE_SIZE, sizeof(char));   /* considered as actual first page for the data */
 
-//     strcat(totalpg_str,"1\n");
+    strcat(totalpg_str,"1\n");
 
-//     fwrite(totalpg_str, sizeof(char), PAGE_SIZE, pagef);
-//     fwrite(first_page, sizeof(char), PAGE_SIZE, pagef);
+    fwrite(totalpg_str, sizeof(char), PAGE_SIZE, pagef);
+    fwrite(first_page, sizeof(char), PAGE_SIZE, pagef);
 
-//     free(totalpg_str);
-//     free(first_page);
+    free(totalpg_str);
+    free(first_page);
 
-//     fclose(pagef);
+    fclose(pagef);
 
-//     return RC_OK;
-// }
+    return 100;
+}

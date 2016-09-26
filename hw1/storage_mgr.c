71df85cf9ae6037962656caa822b0c05c2f6a2b3
Adrianoob <Adrianoob@users.noreply.github.com>
2016-09-26 15:01:40 -0500

    Update storage_mgr.c

--- hw1/storage_mgr.c
+++ hw1/storage_mgr.c
@@ -14,15 +14,16 @@ extern RC createPageFile (char *fileName) {
 	totalpg_str = (char *) calloc(PAGE_SIZE, sizeof(char));  /* allocate "first" page to store total number of pages information */
 	first_page = (char *) calloc(PAGE_SIZE, sizeof(char));   /* considered as actual first page for the data */
 
-    strcat(totalpg_str,"1\n");
+	strcat(totalpg_str,"1\n");
 
-    fwrite(totalpg_str, sizeof(char), PAGE_SIZE, pagef);
-    fwrite(first_page, sizeof(char), PAGE_SIZE, pagef);
 
-    free(totalpg_str);
-    free(first_page);
+	fwrite(totalpg_str, sizeof(char), PAGE_SIZE, pagef);
+	fwrite(first_page, sizeof(char), PAGE_SIZE, pagef);
 
-    fclose(pagef);
+	free(totalpg_str);
+	free(first_page);
 
-    return 100;
+	fclose(pagef);
+
+	return RC_OK;
 }

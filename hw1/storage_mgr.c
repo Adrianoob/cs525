10535d63ab6140a1805845f95a370ca9fc911788
Adrianoob <Adrianoob@users.noreply.github.com>
2016-09-26 17:52:42 -0500

    Update storage_mgr.c

--- hw1/storage_mgr.c
+++ hw1/storage_mgr.c
@@ -10,7 +10,7 @@ extern RC createPageFile (char *fileName) {
 	FILE *newPF = fopen(fileName, "w");
 	char *pf_str;
 
-	pf_str = (char *) malloc(PAGE_SIZE, sizeof(char));  /* allocate "first" page to store total number of pages information */
+	pf_str = (char *) malloc(PAGE_SIZE * sizeof(char));  /* allocate "first" page to store total number of pages information */
 	strcpy(pf_str, "/0");
 
 	fwrite(pf_str, sizeof(char), PAGE_SIZE, newPF);

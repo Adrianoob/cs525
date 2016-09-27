31b1477456e1017b05b96da9345b9ac84f46ece6
Adrianoob <Adrianoob@users.noreply.github.com>
2016-09-26 19:06:48 -0500

    Update storage_mgr.c

--- hw1/storage_mgr.c
+++ hw1/storage_mgr.c
@@ -21,22 +21,38 @@ extern RC createPageFile (char *fileName) {
 	return RC_OK;
 }
 
-RC openPageFile (char *fileName, SM_FileHandle *fHandle){
-    FILE *pf = fopen(fileName, "r+");
+extern RC openPageFile (char *fileName, SM_FileHandle *fHandle){
+	FILE *pf = fopen(fileName, "r+");
+	
+	if (pf){
+		char *str;
 
-    if (pf){
-        char *str;
+		str = (char *) malloc(sizeof(char) * PAGE_SIZE);
+		fgets(str, PAGE_SIZE, pf);
 
-        str = (char *) malloc(sizeof(char) * PAGE_SIZE);
-        fgets(str, PAGE_SIZE, pf);
+		fHandle->fileName = fileName;
+		fHandle->totalNumPages = atoi(str);
+		fHandle->curPagePos = 0;
+		fHandle->mgmtInfo = pf;
+		
+		free(str);
+		return RC_OK;
+	}
+	return RC_FILE_NOT_FOUND;
+}
 
-        fHandle->fileName = fileName;
-        fHandle->totalNumPages = atoi(str);
-        fHandle->curPagePos = 0;
-        fHandle->mgmtInfo = pf;
+extern RC closePageFile (SM_FileHandle *fHandle) {
+	int status = fclose(fHandle->mgmtInfo);
+	if(status == 0){
+		return RC_OK;
+	}
+	return RC_FILE_NOT_FOUND;
+}
 
-        free(str);
-        return RC_OK;
-    }
-    return RC_FILE_NOT_FOUND;
+extern RC destroyPageFile (char *fileName){
+	int status = remove(fileName);
+	if (status == 0){
+		return RC_OK;
+	}
+	return RC_FILE_NOT_FOUND;
 }

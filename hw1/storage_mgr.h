6a270f9cfded64177c1bcefa65e58176c7e88dff
Adrianoob <Adrianoob@users.noreply.github.com>
2016-09-26 19:14:36 -0500

    Update storage_mgr.h

--- hw1/storage_mgr.h
+++ hw1/storage_mgr.h
@@ -22,7 +22,7 @@ extern RC closePageFile (SM_FileHandle *fHandle);
 extern RC destroyPageFile (char *fileName);
 
 // /* reading blocks from disc */
-// extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
 // extern int getBlockPos (SM_FileHandle *fHandle);
 // extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
 // extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
